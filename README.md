c1ffd803d76e68a7acdbe37f8b9336deab734faf
websanova <rob@websanova.com>
2016-05-13 18:47:39 +0700

    Initial commit (v0.1.0).

---
+++ README.md
@@ -0,0 +1,321 @@
+# vue-jwt-auth
+
+Jwt Auth library for Vue.js.
+
+**NOTE:** You must load the `vue-resource` and `vue-router` modules before the `vue-auth` module due to dependencies.
+
+
+## Usage
+
+~~~
+import Auth from './plugins/VueAuth';
+Vue.use(Auth, options);
+~~~
+
+
+## Loading
+
+Since `$auth` is just an instance of `Auth` it can be binded and set to watch. It's good to do this in the root app component.
+
+The `ready()` method can then be used to check when the user is first loaded.
+
+~~~
+<template>
+    <div v-if="$auth.ready()">
+        <ul>
+            <li v-if="$auth.check()">Account</li>
+            <li v-if="!$auth.check()">Login</li>
+        </ul>
+
+        <router-view></router-view>
+    </div>
+
+    <div v-if="!$auth.ready()">
+        Site is loading...
+    </div>
+</template>
+
+<script> 
+    export default {
+        data() {
+            return {
+                $auth: this.$auth
+            };
+        }
+    }
+</script>
+~~~
+
+**NOTE:** This pre-fetch of the user would only occur once on the initial load of the site.
+
+**NOTE:** This will all happen automatically once the package is included. Only the end points for the API will need to be changed which are described in the Options section below.
+
+
+## Privileges
+
+The `vue-jwt-auth` plugin works with the `vue-router` plugin. Setting an `auth` field in the router mapping section will set access to that route.
+
+~~~
+Vue.router.map({
+    '/admin': {
+        auth: 'admin',
+        component: require('./Admin')
+    },
+    '/manage': {
+        auth: ['admin', 'manager'],
+        component: require('./Manage')
+    },
+    '/account': {
+        auth: true,
+        component: require('./Account')
+    },
+    '/login': {
+        auth: false,
+        component: require('./Login')
+    },
+    '/contact': {
+        component: require('./Contact')
+    }
+});
+~~~
+
+
+## Routes
+
+`**true**`
+
+User must be authenticated (no roles are checked).
+
+`**false**`
+
+If the user is logged in then this route will be unavailable. Useful for login/register type pages to be unaccessible once the user is logged in.
+
+`**undefined**`
+
+Public, no checks required.
+
+`**Array**` or `**String**`
+
+The user must be logged in. Additionally the string or array will be checked against the users roles.
+
+Note that the users `roles` variable can be set in the options.
+
+
+## Methods
+
+`**ready()**`
+
+When the app boots the jwt-auth plugin will fire off a check for a token and subsequent user fetch. In the first instance of this the app will be set to "ready".
+
+This will only occur once when you refresh/reload your browser.
+
+`**check()**`
+
+Check if the user is logged in. It can also be used to check if the user has a specific role.
+
+~~~
+$auth.check();
+$auth.check('admin');
+$auth.check(['admin', 'sub-admin']);
+~~~
+
+
+`**user()**`
+
+Fetch the user object stored in the `$auth` instance.
+
+
+`**login()**`
+
+Login the user which accepts four parameters.
+
+~~~
+this.$auth.login(data, rememberMe, redirectURL, {
+    success: function () {
+        console.log('success');
+    },
+    error: function () {
+        console.log('error');
+    }
+});
+~~~
+
+* **data** - Any data to pass on for logging in.
+* **rememberMe** - Should remember the user or not.
+* **redirectUrl** - Which route to redirect to after successful login.
+* **options** - The `success` and `error` callbacks can be set.
+
+`**logout**`
+
+Log the user out.
+
+`**facebook**` `**gogole**`
+
+These follows the same format as the `$auth.login()` method.
+
+However the data for these works a bit differently. All the options for social authentication can be set in the `options` however they can also be set directly here in the `data` variable.
+
+* **appId** - Set the social appId.
+* **scope** - Scope of permissions required.
+* **redirect** - Set the redirect url.
+* **state** - Any additional state fields which will be passed back to the app.
+* **code** - Pass the code in to verify with the api. This will automatically fire when the code field is detected.
+
+~~~
+var data = {
+    appId: '11111111111111111',
+    scope: 'email'
+};
+
+this.auth.facebook(data, true, '/account');
+~~~
+
+~~~
+var data = {
+    code: this.$route.query.code
+};
+
+var callbacks = {
+    success: function () {},
+    error: function () {}
+};
+
+this.auth.facebook(data, null, null, callbacks);
+~~~
+
+
+## Options
+
+**NOTE:** All the ur's here are relative. It is assumed the vue app will have some scheme for auto pre-pending the full path to the routes.
+
+`**authType**`         
+
+**default:** 'bearer'
+
+Set the auth type. Only `bearer` is supported at this time.
+
+`**fetchUrl**`         
+
+**default:** '/auth/user'
+
+The url for fetching the user from the api.
+
+`**tokenUrl**`         
+
+**default:** '/auth/token'
+
+The url for fetching a new token from the api.
+
+`**loginUrl**`         
+
+**default:** '/auth/login'
+
+The url for logging in.
+
+`**registerUrl**`      
+
+**default:** '/auth/register'
+
+Url method for registering a user.
+
+`**loginRedirect**`    
+
+**default:** '/login'
+
+Redirect to this url if the user does not have authorization to the route.
+
+`**notFoundRedirect**` 
+
+**default:** '/404'
+
+Redirect to this url if the route is not accessible (this is in the case of setting a route to `false`). It will not handle 404 redirects for the entire app.
+
+`**forbiddenRedirect**`
+
+**default:** '/403'
+
+Redirect to this url if the user does not have access to the route.
+
+`**rolesVar**`         
+
+**default:** 'roles'
+
+The name of the field that contains the roles in the user object returned from the api.
+
+`**tokenVar**`         
+
+**default:** 'token'
+
+The name of the field that contains the token returned from the api.
+
+`**tokenName**`        
+
+**default:** 'jwt-auth-token'
+
+The name of the token stored in local storage.
+
+`**cookieDomain**`     
+
+**default:** _cookieDomain
+
+The domain to use for the cookie. Can also override the local method for this.
+
+`**userData**`         
+
+**default:** _userData
+
+Locally this will simply return `data` on the response object. The local method can be overridden.
+
+`**beforeEach**`       
+
+**default:** _beforeEach
+
+This method runs before each request and handles the logic for permissions to a route. If another scheme is needed it can be overridden.
+
+`**facebookUrl**`      
+
+**default:** '/auth/facebook'
+
+The url for processing the Facebook token code in the api.
+
+`**facebookAppId**`
+
+**default:** ''
+
+Set the Facebook client app id.
+
+`**facebookScope**`    
+
+**default:** 'email'
+
+Set the permissions scope required for the app from Facebook.
+
+`**facebookRedirect**` 
+
+**default:** _getUrl() + '/login/facebook'
+
+Set the redirect url to return to your app after oauth.
+
+`**googleUrl**`        
+
+**default:** '/auth/google'
+
+The url for processing the Google token code in the api.
+
+`**googleAppId**`      
+
+**default:** ''
+
+Set the Google client app id.
+
+`**googleScope**`      
+
+**default:** 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+
+Set the permissions scope required for the app from Google.
+
+`**googleRedirect**`   
+
+**default:** _getUrl() + '/login/google'
+
+Set the redirect url to return to your app after oauth.
---
+++ vue-jwt-auth.js
@@ -0,0 +1,358 @@
+/*
+    https://github.com/websanova/vue-jwt-auth
+    rob@websanova.com
+    Version 0.1.0
+ */
+
+module.exports = (function () {
+
+    var _ctx = null;
+
+    function Auth(options) {
+        this.authenticated = null;
+        this.loaded = false;
+        this.data = null;
+
+        this.options = {
+            authType          : options.authType          || 'bearer',
+
+            fetchUrl          : options.fetchUrl          || '/auth/user',
+            tokenUrl          : options.tokenUrl          || '/auth/token',
+            loginUrl          : options.loginUrl          || '/auth/login',
+            // registerUrl       : options.registerUrl       || '/auth/register',
+
+            loginRedirect     : options.loginRedirect     || '/login',
+            notFoundRedirect  : options.notFoundRedirect  || '/404',
+            forbiddenRedirect : options.forbiddenRedirect || '/403',
+
+            rolesVar          : options.rolesVar          || 'roles',
+            tokenVar          : options.tokenVar          || 'token',
+            tokenName         : options.tokenName         || 'jwt-auth-token',
+            
+            cookieDomain      : options.cookieDomain      || _cookieDomain,
+            userData          : options.userData          || _userData,
+            beforeEach        : options.beforeEach        || _beforeEach,
+            
+            facebookUrl       : options.facebookUrl       || '/auth/facebook',
+            facebookAppId     : options.facebookAppId     || '',
+            facebookScope     : options.facebookScope     || 'email',
+            facebookRedirect  : options.facebookRedirect  || _getUrl() + '/login/facebook',
+            
+            googleUrl         : options.googleUrl         || '/auth/google',
+            googleAppId       : options.googleAppId       || '',
+            googleScope       : options.googleScope       || 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read',
+            googleRedirect    : options.googleRedirect    || _getUrl() + '/login/google'
+        };
+    }
+
+    function _beforeEach(transition) {
+        var roles,
+            auth = _toArray(transition.to.auth);
+
+        if (auth && (auth === true || auth.constructor === Array)) {
+            if ( ! this.check()) {
+                _ctx.$router.replace(this.options.loginRedirect);
+            }
+            else if (auth.constructor === Array && ! _compare(auth, _toArray(this.user()[this.options.rolesVar]))) {
+                _ctx.$router.replace(this.options.forbiddenRedirect);
+            }
+            else {
+                return transition.next();
+            }
+        }
+        else if (auth === false && this.check()) {
+            _ctx.$router.replace(this.options.notFoundRedirect);
+        }
+        else {
+            return transition.next();
+        }
+    }
+
+    function _userData(resp) {
+        return resp.data;
+    }
+
+    function _cookieDomain() {
+        return window.location.hostname;
+    }
+
+    function _getToken() {
+        return localStorage.getItem(this.options.tokenName);
+    }
+
+    function _setToken(token) {
+        if (token) {
+            localStorage.setItem(this.options.tokenName, token);
+        }
+    }
+
+    function _removeToken() {
+        localStorage.removeItem(this.options.tokenName);
+    }
+
+    function _setRememberMeCookie(rememberMe) {
+        _setCookie.call(this,
+            'rememberMe',
+            rememberMe === true ? 'true' : 'false',
+            rememberMe === true ? 12096e5 : undefined
+        );
+    }
+
+    function _removeRememberMeCookie() {
+        _setCookie.call(this, 'rememberMe', 'false', -12096e5);
+    }
+
+    function _login(path, data, rememberMe, redirectUrl, options) {
+        var _this = this;
+
+        options = options || {};
+
+        _ctx.$http.post(path, data, function(resp) {
+            _setRememberMeCookie.call(_this, rememberMe);
+
+            _setToken.call(_this, resp[_this.options.tokenVar]);
+
+            _this.authenticated = null;
+            
+            if (options.success) {
+                options.success.call(_ctx);
+            }
+
+            _this.fetch(function () {
+                if (redirectUrl && _this.check()) {
+                    _ctx.$router.go(redirectUrl);
+                }
+            });
+        }, {
+            error: function () {
+                if (options.error) {
+                    options.error.call(_ctx);
+                }
+            }
+        });
+    }
+
+    function _refreshToken() {
+        if (_getToken.call(this)) {
+            _ctx.$http.get(this.options.tokenUrl);
+        }
+    }
+
+    function _social(type, data, rememberMe, redirectUrl, options) {
+        var _this  = this,
+            state,
+            params = '';
+
+        if (data.code) {
+            state = JSON.parse(_ctx.$route.query.state);
+
+            _login.call(this, this.options[type + 'Url'], data, state.rememberMe, state.redirect, options);
+        }
+        else {
+            data.state = data.state || {};
+            data.state.rememberMe = rememberMe === true ? true : false;
+            data.state.redirect = redirectUrl ? redirectUrl : '';
+
+            data.appId = data.appId || this.options[type + 'AppId'];
+            data.scope = data.scope || this.options[type + 'Scope'];
+            data.redirect = data.redirect || this.options[type + 'Redirect'];
+
+            params = '?client_id=' + data.appId + '&redirect_uri=' + data.redirect + '&scope=' + data.scope + '&response_type=code&state=' + JSON.stringify(data.state);
+
+            if (type === 'facebook') {
+                window.location = 'https://www.facebook.com/v2.5/dialog/oauth' + params;
+            }
+            else if (type === 'google') {
+                window.location = 'https://accounts.google.com/o/oauth2/auth' + params;
+            }
+        }
+    }
+
+    function _getUrl() {
+        var port = window.location.port;
+
+        return window.location.protocol + '//' + window.location.hostname + (port ? ':' + port : '');
+    }
+
+    function _setCookie(name, value, timeOffset) {
+        var domain  = this.options.cookieDomain.call(this),
+            expires = (new Date((new Date).getTime() + timeOffset)).toUTCString();
+
+        document.cookie = (name + '=' + value + '; domain=' + domain + '; path=/;' + (timeOffset ? ' expires=' + expires + ';' : '') );
+    }
+
+    function _compare(one, two) {
+        var i, ii;
+
+        one = _toArray(one);
+        two = _toArray(two);
+
+        if (one.constructor !== Array || two.constructor !== Array) {
+            return false;
+        }
+
+        for (i = 0, ii = one.length; i < ii; i++) {
+            if (two.indexOf(one[i]) >= 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function _toArray(val) {
+        return (typeof val) === 'string' ? [val] : val;
+    }
+
+    Auth.prototype.fetch = function(cb) {
+        var _this = this;
+
+        cb = cb || function () {};
+
+        if ( ! this.loaded) {
+            _refreshToken.call(this);
+        }
+
+        if (this.authenticated === null && _getToken.call(this)) {
+            if ( ! document.cookie.match(/rememberMe/)) {
+                _removeToken.call(this);
+                _this.loaded = true;
+                
+                return cb();
+            }
+
+            this.authenticated = false;
+            
+            _ctx.$http.get('/auth/user', function(resp) {
+                _this.authenticated = true;
+                _this.data = _this.options.userData.call(_this, resp);
+                _this.loaded = true;
+
+                return cb();
+            }, {
+                error: function() {
+                    _this.loaded = true;
+
+                    return cb();
+                }
+            });
+        }
+        else {
+            _this.loaded = true;
+
+            return cb();
+        }        
+    };
+
+    Auth.prototype.context = function(ctx) {
+        if (ctx) { _ctx = ctx; }
+
+        return _ctx;
+    };
+
+    Auth.prototype.token = function (token) {
+        if (token) { _setToken.call(this, token); }
+
+        return _getToken.call(this);
+    };
+
+    Auth.prototype.ready = function() {
+        return this.loaded;
+    };
+
+    Auth.prototype.check = function(role) {
+        if (this.data !== null) {
+            if (role) {
+                return _compare(role, this.data[this.options.rolesVar]);
+            }
+
+            return true;
+        }
+
+        return false;
+    };
+
+    Auth.prototype.user = function() {
+        return this.data;
+    };
+
+    Auth.prototype.login = function(data, rememberMe, redirectUrl, options) {
+        _login.call(this, this.options.loginUrl, data, rememberMe, redirectUrl, options);
+    };
+
+    Auth.prototype.facebook = function(data, rememberMe, url, options) {
+        _social.call(this, 'facebook', data, rememberMe, url, options);
+    };
+
+    Auth.prototype.google = function(data, rememberMe, url, options) {
+        _social.call(this, 'google', data, rememberMe, url, options);
+    };
+
+    Auth.prototype.logout = function(url) {
+        _removeRememberMeCookie.call(this);
+        _removeToken.call(this);
+
+        this.authenticated = false;
+        this.data = null;
+
+        if (!this.check()) {
+            _ctx.$router.go(url || '/');
+        }
+    };
+
+    return function install(Vue, options) {
+        var auth = new Auth(options);
+
+        Vue.auth = Auth;
+
+        Object.defineProperties(Vue.prototype, {
+            $auth: {
+                get: function () {
+                    auth.context(this);
+
+                    return auth;
+                }
+            }
+        });
+
+        // Setup before each route change check.
+        Vue.router.beforeEach(function (transition) {
+
+            // Make sure to use $auth.fetch so context is loaded.
+            transition.to.router.app.$auth.fetch(function () {
+                auth.options.beforeEach.bind(auth)(transition);
+            });
+        });
+
+        Vue.http.interceptors.push({
+
+            // Send auth token on each request.
+            request: function (req) {
+                var token = auth.token();
+
+                if (token) {
+                    if (auth.options.authType === 'bearer') {
+                        req.headers.Authorization = 'Bearer: ' + token;
+                    }
+                }
+
+                return req;
+            },
+
+            // Reset auth token if provided in response.
+            response: function (res) {
+                var authorization = res.headers('authorization');
+
+                if (authorization) {
+                    authorization = authorization.split(' ');
+
+                    if (authorization[1]) {
+                        _setToken.call(auth, authorization[1]);
+                    }
+                }
+
+                return res;
+            }
+        });
+    }
+})();
