5fada78d5d4454051d750b7e501996f17e349b5a
Roman Pisarew <roman.pisarew@gmail.com>
2016-12-23 15:07:25 +0100

    add a way to set a custom axios instance

--- drivers/http/axios.js
+++ drivers/http/axios.js
@@ -1,44 +1,51 @@
-var axios = require('axios');
+module.exports = function (axiosInstance) {
+  var axios = null
 
-module.exports = {
+  if (axiosInstance) {
+    axios = axiosInstance
+  } else {
+    axios = require('axios');
+  }
 
-  _interceptor: function (req, res) {
-    let _this = this;
+  return {
+    _interceptor: function (req, res) {
+      var _this = this;
 
-    if (req) {
-      axios.interceptors.request.use((request) => {
-        req.call(_this, request);
-        return request;
-      })
+      if (req) {
+        axios.interceptors.request.use((request) => {
+          req.call(_this, request);
+          return request;
+        })
+      }
+
+      if (res) {
+        axios.interceptors.response.use((response) => {
+          res.call(_this, response);
+          return response;
+        })
+      }
+    },
+
+    _invalidToken: function (res) {
+      if (res.status === 401) {
+        this.logout();
+      }
+    },
+
+    _httpData: function (res) {
+      return res.data || {};
+    },
+
+    _http: function (data) {
+      axios(data).then(data.success, data.error);
+    },
+
+    _getHeaders: function (res) {
+      return res.headers;
+    },
+
+    _setHeaders: function (req, headers) {
+      req.headers.common = Object.assign(req.headers.common, headers);
     }
-
-    if (res) {
-      axios.interceptors.response.use((response) => {
-        res.call(_this, response);
-        return response;
-      })
-    }
-  },
-
-  _invalidToken: function (res) {
-    if (res.status === 401) {
-      this.logout();
-    }
-  },
-
-  _httpData: function (res) {
-    return res.data || {};
-  },
-
-  _http: function (data) {
-    axios(data).then(data.success, data.error);
-  },
-
-  _getHeaders: function (res) {
-    return res.headers;
-  },
-
-  _setHeaders: function (req, headers) {
-    req.headers.common = Object.assign(req.headers.common, headers);
   }
 }
