e3a90eaa84b873b273fd3997617c4dcf216b5235
Roman Pisarew <roman.pisarew@gmail.com>
2017-01-10 23:31:31 +0100

    axios driver now use the vue-axios plugin, to get the axios instance

--- drivers/http/axios.js
+++ drivers/http/axios.js
@@ -1,51 +1,47 @@
-module.exports = function (axiosInstance) {
-  var axios = null
-
-  if (axiosInstance) {
-    axios = axiosInstance
-  } else {
-    axios = require('axios');
-  }
-
-  return {
-    _interceptor: function (req, res) {
-      var _this = this;
-
-      if (req) {
-        axios.interceptors.request.use((request) => {
-          req.call(_this, request);
-          return request;
-        })
+module.exports = {
+  _init: function () {
+      if ( ! this.options.Vue.axios) {
+          return 'axios.js : Vue.axios must be set.'
       }
+  },
+  
+  _interceptor: function (req, res) {
+    var _this = this;
 
-      if (res) {
-        axios.interceptors.response.use((response) => {
-          res.call(_this, response);
-          return response;
-        })
-      }
-    },
-
-    _invalidToken: function (res) {
-      if (res.status === 401) {
-        this.logout();
-      }
-    },
-
-    _httpData: function (res) {
-      return res.data || {};
-    },
-
-    _http: function (data) {
-      axios(data).then(data.success, data.error);
-    },
-
-    _getHeaders: function (res) {
-      return res.headers;
-    },
-
-    _setHeaders: function (req, headers) {
-      req.headers.common = Object.assign(req.headers.common, headers);
+    if (req) {
+      this.options.Vue.axios.interceptors.request.use((request) => {
+        req.call(_this, request);
+        return request;
+      })
     }
+
+    if (res) {
+      this.options.Vue.axios.interceptors.response.use((response) => {
+        res.call(_this, response);
+        return response;
+      })
+    }
+  },
+
+  _invalidToken: function (res) {
+    if (res.status === 401) {
+      this.logout();
+    }
+  },
+
+  _httpData: function (res) {
+    return res.data || {};
+  },
+
+  _http: function (data) {
+    this.options.Vue.axios(data).then(data.success, data.error);
+  },
+
+  _getHeaders: function (res) {
+    return res.headers;
+  },
+
+  _setHeaders: function (req, headers) {
+    req.headers.common = Object.assign(req.headers.common, headers);
   }
 }
