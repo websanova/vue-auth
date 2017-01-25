2f449224395b938b982d0ba306e2540102d95a1f
Roman Pisarew <roman.pisarew@gmail.com>
2017-01-25 18:51:30 +0100

    removed arrow functions

--- drivers/http/axios.js
+++ drivers/http/axios.js
@@ -4,19 +4,19 @@ module.exports = {
           return 'axios.js : Vue.axios must be set.'
       }
   },
-  
+
   _interceptor: function (req, res) {
     var _this = this;
 
     if (req) {
-      this.options.Vue.axios.interceptors.request.use((request) => {
+      this.options.Vue.axios.interceptors.request.use(function (request) {
         req.call(_this, request);
         return request;
       })
     }
 
     if (res) {
-      this.options.Vue.axios.interceptors.response.use((response) => {
+      this.options.Vue.axios.interceptors.response.use(function (response) {
         res.call(_this, response);
         return response;
       })
