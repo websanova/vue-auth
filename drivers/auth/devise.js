98308df9abb4788bfe4ddf173a6db922ef9d107d
Artem Chernyak <artemchernyak@gmail.com>
2017-04-23 20:16:46 -0500

    backward compatibility

--- drivers/auth/devise.js
+++ drivers/auth/devise.js
@@ -1,5 +1,6 @@
 module.exports = {
-    tokens: ['token-type', 'access-token', 'client', 'uid', 'expiry'],
+    tokens: ['Token-Type', 'Access-Token', 'Client', 'Uid', 'Expiry',
+        'token-type', 'access-token', 'client', 'uid', 'expiry'],
 
     request: function (req, token) {
         var headers = {},
@@ -18,8 +19,9 @@ module.exports = {
         var token = [],
             headers = this.options.http._getHeaders.call(this, res);
         
-        if (headers['access-token']) {
-            this.options.auth.tokens.forEach(function (tokenName) {
+        if (headers['access-token'] || headers['Access-Token']) {
+            var auth =  this.options.deviseAuth || this.options.auth;
+            auth.tokens.forEach(function (tokenName) {
                 if (headers[tokenName]) {
                     token.push(headers[tokenName]);
                 }
