df490dc185ecf8f806bf080842795fcb150fbea7
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-06-04 12:19:26 +0300

    fixed post body parameter
    login method expect `data` parameter, i.e. `xhr.js` uses: `var requestData = config.data;`
    ps: tested with vue-axios

--- 2.x.demo/src/components/pages/Login.vue
+++ 2.x.demo/src/components/pages/Login.vue
@@ -61,7 +61,7 @@
                 var redirect = this.$auth.redirect();
 
                 this.$auth.login({
-                    body: this.data.body,
+                    data: this.data.body,
                     rememberMe: this.data.rememberMe,
                     redirect: {name: redirect ? redirect.from.name : 'account'},
                     fetchUser: this.data.fetchUser,
