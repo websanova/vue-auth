134758dd0a2fde7b1d88fce41cb69f23da6b4a54
websanova <rob@websanova.com>
2020-03-28 11:13:15 +0700

    Update token store order.

--- src/auth.js
+++ src/auth.js
@@ -479,7 +479,7 @@ module.exports = function () {
         rolesVar:             'roles',
         tokenImpersonateName: 'impersonate_auth_token',
         tokenDefaultName:     'default_auth_token',
-        tokenStore:           ['sessionStorage', 'localStorage', 'cookie'],
+        tokenStore:           ['localStorage', 'cookie', 'sessionStorage'],
 
         // Objects
 
