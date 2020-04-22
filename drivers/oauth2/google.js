e728daedf472911b5a096ff64affcefa1e1998e1
websanova <rob@websanova.com>
2020-04-22 17:31:26 +0700

    Update default google oauth2 scope to email.

--- drivers/oauth2/google.js
+++ drivers/oauth2/google.js
@@ -5,7 +5,7 @@ export default {
         client_id: '',
         redirect_uri: 'login/google',
         response_type: 'code',
-        scope: 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read',
+        scope: 'email',
         state: {},
     }
 }
