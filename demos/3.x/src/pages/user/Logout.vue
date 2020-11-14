a4b431abac2efcd7c17d2d78063ac0fc1bd4aa70
websanova <rob@websanova.com>
2020-11-14 11:02:41 +0700

    Setup logout page.

--- demos/3.x/src/pages/user/Logout.vue
+++ demos/3.x/src/pages/user/Logout.vue
@@ -1,5 +1,81 @@
 <template>
-    <div>
-        logout
+    <div class="container container-sm">
+        <ul class="spacer">
+            <li>
+                <button @click="logoutRedirect">
+                    Redirect
+                </button>
+            </li><li>
+                <button @click="logoutThen">
+                    Then
+                </button>
+            </li><li>
+                <button @click="logoutRequest">
+                    Request
+                </button>
+            </li><li>
+                <button @click="logoutDefault">
+                    Default
+                </button>
+            </li><li>
+                <button @click="logoutVuex">
+                    Vuex
+                </button>
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+    import {useStore } from 'vuex';
+    import {useRouter} from 'vue-router';
+    import {useAuth  } from '@websanova/vue-auth/src/v3.js';
+
+    export default {
+        setup() {
+            const auth   = useAuth();
+            const store  = useStore();
+            const router = useRouter();
+
+            function logoutDefault() {
+                auth.logout();
+            }
+
+            function logoutRedirect() {
+                auth
+                    .logout({
+                        redirect: {name: 'auth-login'}
+                    });
+            }
+
+            function logoutThen() {
+                auth
+                    .logout({
+                        redirect: null
+                    })
+                    .then(() => {
+                        router.push({name: 'auth-login'});
+                    });
+            }
+
+            function logoutVuex() {
+                store.dispatch('auth/logout');
+            }
+            
+            function logoutRequest() {
+                auth
+                    .logout({
+                        makeRequest: true
+                    });
+            }
+
+            return {
+                logoutDefault,
+                logoutRedirect,
+                logoutThen,
+                logoutVuex,
+                logoutRequest,
+            }
+        }
+    }
+</script>
