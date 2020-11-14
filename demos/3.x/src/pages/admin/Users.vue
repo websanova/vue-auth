abdaef8fff4307350c83306032f551177272a8b8
websanova <rob@websanova.com>
2020-11-14 11:27:54 +0700

    Admin users and unimpersonate.

--- demos/3.x/src/pages/admin/Users.vue
+++ demos/3.x/src/pages/admin/Users.vue
@@ -1,5 +1,109 @@
 <template>
     <div>
-        Users
+        <div
+            v-show="state.form.status !== 'success'"
+            class="spinner"
+        >
+            Loading users...
+        </div>
+
+        <div
+            v-if="state.form.status === 'success'"
+        >
+            <div
+                v-for="user in state.form.data.items"
+                class="media"
+            >
+
+                <div class="media-middle">
+
+                    <span class="text-muted">
+                        ({{ user.role }})
+                    </span>
+
+                    &nbsp;
+
+                    {{ user.first_name }}
+                </div>
+
+                <div class="media-tight media-middle">
+                    <ul class="spacer">
+                        <li>
+                            <button
+                                @click="impersonateDefault(user)"
+                            >
+                                Default
+                            </button>
+                        </li><li>
+                            <button
+                                @click="impersonateVuex(user)"
+                            >
+                                Vuex
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+    import {reactive } from 'vue';
+    import {onMounted} from 'vue';
+    import {useStore } from 'vuex';
+    import axios       from 'axios';
+    import {useAuth  } from '@websanova/vue-auth/src/v3.js';
+
+    export default {
+        setup() {
+            const auth   = useAuth();
+            const store  = useStore();
+
+            const state = reactive({
+                form: {
+                    status: null,
+                    data: null
+                }
+            });
+
+            onMounted(() => {
+                getUsers();
+            });
+
+            function getUsers() {
+                state.form.status = 'loading';
+
+                axios({
+                    url: 'users/list'
+                })
+                .then((res) => {
+                    state.form.data = res.data.data;
+
+                    state.form.status = 'success';
+                });
+            }
+
+            function impersonateDefault(user) {
+                auth
+                .impersonate({
+                    url: 'auth/' + user.id + '/impersonate',
+                    redirect: {name: 'user-account'},
+                });
+            }
+
+            function impersonateVuex(user) {
+                store
+                .dispatch('auth/impersonate', {
+                    user: user
+                });
+            }
+
+            return {
+                state,
+                impersonateVuex,
+                impersonateDefault,
+            }
+        }
+    }
+</script>
--- demos/3.x/src/pages/user/Unimpersonate.vue
+++ demos/3.x/src/pages/user/Unimpersonate.vue
@@ -1,5 +1,81 @@
 <template>
-    <div>
-        unimpersonate
+    <div class="container container-sm">
+        <ul class="spacer">
+            <li>
+                <button @click="unimpersonateRedirect">
+                    Redirect
+                </button>
+            </li><li>
+                <button @click="unimpersonateThen">
+                    Then
+                </button>
+            </li><li>
+                <button @click="unimpersonateRequest">
+                    Request
+                </button>
+            </li><li>
+                <button @click="unimpersonateDefault">
+                    Default
+                </button>
+            </li><li>
+                <button @click="unimpersonateVuex">
+                    Vuex
+                </button>
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+    import {useStore } from 'vuex';
+    import {useRouter} from 'vue-router';
+    import {useAuth  } from '@websanova/vue-auth/src/v3.js';
+
+    export default {
+        setup() {
+            const auth   = useAuth();
+            const store  = useStore();
+            const router = useRouter();
+
+            function unimpersonateDefault() {
+                auth.unimpersonate();
+            }
+
+            function unimpersonateRedirect() {
+                auth
+                .unimpersonate({
+                    redirect: {name: 'admin-users'}
+                });
+            }
+
+            function unimpersonateThen() {
+                auth
+                .unimpersonate({
+                    redirect: null
+                })
+                .then(() => {
+                    router.push({name: 'admin-users'});
+                });
+            }
+
+            function unimpersonateVuex() {
+                store.dispatch('auth/unimpersonate');
+            }
+            
+            function unimpersonateRequest() {
+                auth
+                .unimpersonate({
+                    makeRequest: true
+                });
+            }
+
+            return {
+                unimpersonateThen,
+                unimpersonateVuex,
+                unimpersonateRequest,
+                unimpersonateDefault,
+                unimpersonateRedirect,
+            };
+        }
+    }
+</script>
